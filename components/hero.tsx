b0293117cd4d63caaf527b8d77e3c7e5dc472baf
simo-sari07 <simosari100@gmail.com>
2025-12-03 21:55:52 +0100

    errew

--- components/hero.tsx
+++ components/hero.tsx
@@ -287,7 +287,7 @@ export default function Hero({ isLoaded = true }) {
               </div>
 
               <div className="text-left">
-                <p className="text-xs font-semibold text-black">368 Reviews</p>
+                <p className="text-xs font-semibold text-black">86 Reviews</p>
                 <p className="text-sm font-bold text-[#7a9898] flex items-center gap-1">
                   {"★★★★★".split("").map((star, i) => (
                     <motion.span
