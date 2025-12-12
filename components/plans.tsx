7b41898e6bca1fe949b79c748d2ec07f5b6a8ddc
simo-sari07 <150151322+simo-sari07@users.noreply.github.com>
2025-12-13 00:51:32 +0100

    Update plans.tsx

--- components/plans.tsx
+++ components/plans.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react"
 const plans = [
   {
     name: "1 Month",
-    price: "50",
+    price: "23.99",
     period: "/month",
     badge: "Starter",
     description: "Begin your journey",
